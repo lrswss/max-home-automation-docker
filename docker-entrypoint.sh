28c1576ebc16e0467694b4caf1e613591a63b170
root <root@wsspi.bytebox.local>
2022-02-02 00:03:27 +0100

    update container config and entry-point script
    - let x11vnc start max_home_automation through .xinitrc
    - xvfb/x11vnc requires container to run privileged
    - restart container unless stopped

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,11 +5,9 @@ XAUTHLOCALHOSTNAME=localhost
 set -Eeo pipefail
 
 if [ "$1" = "x11vnc" ]; then
-	echo -n "Starting max_home_automation on virtual framebuffer "
-	xvfb-run -a -n 10 -f /tmp/xvfb.xauth max-home-automation.sh &
-	sleep 2
-	echo "Starting x11vnc on port 5900..."
-	x11vnc -rfbport 5900 -display :10 -forever -nopw -noxkb -auth /tmp/xvfb.xauth >/dev/null 2>&1
+	echo "max-home-automation.sh" > ~/.xinitrc
+	chmod 755 ~/.xinitrc
+	x11vnc -rfbport 5900 -create -forever -nopw -noxkb
 else
 	exec "$@"
 fi
